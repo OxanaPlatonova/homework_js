f06e99a5239e8a4c1046db37515b0c5c257db7ba
unit6 <unit6@taurussoft.net>
2018-02-05 15:46:38 +0200

    home_7

--- home_7/script.js
+++ home_7/script.js
@@ -1,4 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {	
+	var ball = document.getElementsByClassName("ball");	
+	ball[0].style.left = 50 + "px";	
+	ball[1].style.left = screen.availWidth/2 - 50 + 'px';
+	ball[2].style.left = screen.availWidth/4 - 50 + 'px';
+	ball[3].style.left = screen.availWidth - 100 + 'px';
+	ball[4].style.left = screen.availWidth/5 - 50 + 'px';
+	ball[5].style.left = screen.availWidth/3 - 50 + 'px';	
+	ball[0].style.top = "0";
+	ball[1].style.top = "0";
+	ball[2].style.top = "0";
+	ball[3].style.top = "0";
+	ball[4].style.top = "0";
+	ball[5].style.top = "0";
+	(function moveBall(){
+		let i = 0;	
+		let stepTop = 1;
+		let start = setInterval (function (){
+			if(ball[i].style.top == screen.availHeight + 'px'){				
+				i++;
+				ball[i-1].style.top = "0";
+
+			}
+			if(i == 6){
+				i = 0;
+			}else{
+				ball[i].style.top = parseInt(ball[i].style.top.slice(0, -2)) + stepTop + 'px'
+			}	
+		},1);
+	}());
 	var elem = document.getElementById("cube");
 	elem.style.bottom = "0";
 	elem.style.left = screen.availWidth / 2 - 75 + "px";
@@ -7,29 +36,9 @@ document.addEventListener("DOMContentLoaded", function() {
 		function moveAt(e) {  			
 			elem.style.left = e.pageX - elem.offsetWidth/2 + 'px';		  	  	
 		}
-		document.onmousemove = function(e) {
+		document.addEventListener('mousemove', function(e){
 		moveAt(e);
-		}
+		}, false);
 	}, false);
-	var ball = document.getElementsByClassName("ball");	
-	ball[0].style.left = "0";
-	ball[1].style.left = screen.availWidth/2 - 50 + 'px';
-	ball[2].style.left = screen.availWidth/4 - 50 + 'px';
-	ball[3].style.left = screen.availWidth - 50 + 'px';
-	ball[4].style.left = screen.availWidth/5 - 50 + 'px';
-	ball[5].style.left = screen.availWidth/3 - 50 + 'px';
-	var i = 0;
-	ball[0].style.top = "0";
-	(function moveBall(){	
-		let stepTop = 1;	
-		let height = Math.max(screen.availHeight || 0);
-		let start = setInterval (function (){
-			if(ball[i].style.top == screen.availHeight + 'px'){
-				ball[i].style.top = "0";
-			}else{
-				ball[i].style.top = parseInt(ball[i].style.top.slice(0, -2)) + stepTop + 'px'
-			}	
-		},1);
-	}());	
 });
 
